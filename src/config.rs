68da3189f704fa59a98a768a8d4101df87463fc9
Zach Probst <zprobst@resilientvitality.com>
2021-05-25 21:33:40 -0400

    Add some small docs to add comments to the CLI help screen

--- src/config.rs
+++ src/config.rs
@@ -8,11 +8,14 @@ use structopt::StructOpt;
 )]
 pub struct Config {
     #[structopt(short, long)]
+    /// Print extra debug information.
     pub debug: bool,
 
     #[structopt(parse(from_os_str))]
+    /// The folder to mount or migrate.
     pub folder: PathBuf,
 
     #[structopt(short, long)]
+    /// Migrates an unencrpyted folder to an encrypted volume managed through dot-protector.
     pub migrate: bool,
 }
